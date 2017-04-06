b0a4f2312a96c8ad58f38aef59169dc551f4d075
Nick Strayer <nstrayer@uvm.edu>
2017-04-06 17:59:24 -0500

    Added docs to the alr viz function

--- R/alr_results.R
+++ R/alr_results.R
@@ -1,7 +1,16 @@
 # Takes an alternating logistic regression fit from the ordgee function
 # and generates a nice table with plots of the coefficient values and their confidence intervals.
-
-alr_results <- function(model_fit, title){
+#' Visualize Results of Alternating Logistic Regressions.
+#'
+#' Takes output from the `ordgee` function in the package `geepack` and returns a table with visual results along with numerical values imposed on the graph
+#'
+#' @param model_fit an object returned by `ordgee`
+#' @param title Text for the title portion of your results
+#' @param included_models which model you want to show in results. Defaults to both mean and correlation. For mean enter "mean", correlation: "correlation".
+#' @export
+#' @examples
+#' alr_results(my_fit, "Results from fitting alternating logistic regression", included_models = "both")
+alr_results <- function(model_fit, title, included_models = "both"){
   model_summary <- summary(model_fit)
 
   #Grab the mean model estimates and standard errors
@@ -10,9 +19,25 @@ alr_results <- function(model_fit, title){
   #Grab the correlation model's as well
   corr_model_params <- model_summary$correlation[,c(1,2)] %>%
     mutate(model_type = "correlation model", variable = row.names(.))
-  #combine the two
-  model_params <- rbind(mean_model_params, corr_model_params) %>%
-    mutate(model_type = fct_inorder(model_type))
+
+  if(included_models == "both"){
+    #combine the two
+    model_params <- rbind(mean_model_params, corr_model_params) %>%
+      mutate(model_type = fct_inorder(model_type))
+
+    our_subtitle = "The 'mean model' panel contains estimates of the effect of a given variable's one unit increase\n(holding all other variables constant) on the log-odds of event occuring at a given measurement.\n\nThe 'correlation model' panel shows a variable's effect on the log-odds of an event occuring given\nan event occured at another measurement.\n"
+  } else if(included_models == "mean"){
+    #combine the two
+    model_params <- mean_model_params %>%
+      mutate(model_type = fct_inorder(model_type))
+    our_subtitle = "The 'mean model' panel contains estimates of the effect of a given variable's one unit increase\n(holding all other variables constant) on the log-odds of event occuring at a given measurement.\n"
+  } else {
+    model_params <- corr_model_params %>%
+      mutate(model_type = fct_inorder(model_type))
+
+    our_subtitle = "The 'correlation model' panel shows a variable's effect on the log-odds of an event occuring\ngiven an event occured at another measurement.\n"
+  }
+
 
   ggplot(model_params, aes(y = estimate,x = variable, color = variable)) +
     geom_hline(yintercept = 0, alpha = 0.2)+
@@ -24,5 +49,5 @@ alr_results <- function(model_fit, title){
     facet_wrap(~model_type, scales = "free_y") +
     coord_flip() +
     scale_colour_brewer(palette="Paired") +
-    labs(y = "coefficient estimate", title = title, subtitle = "The 'mean model' panel contains estimates of the effect of a given variable's one unit increase\n(holding all other variables constant) on the log-odds of infection occuring at a given measurement.\n\nThe 'correlation model' panel shows a variable's effect on the log-odds of an infection occuring given\nan infection occured at another measurement.\n")
+    labs(y = "coefficient estimate", title = title, subtitle = our_subtitle)
 }
---
+++ R/src.R
@@ -0,0 +1,7 @@
+`%>%`      <- magrittr::`%>%`
+mutate     <- dplyr::mutate
+rename     <- dplyr::rename
+right_join <- dplyr::right_join
+filter     <- dplyr::filter
+select     <- dplyr::select
+group_by   <- dplyr::group_by
